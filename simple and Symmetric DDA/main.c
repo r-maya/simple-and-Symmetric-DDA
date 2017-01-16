ba4b0e49431f9a8cfecacbe48d8f35a081788ec8
B Raghu Ram Reddy <RaghuRRB@RAS.local>
2017-01-16 17:19:44 +0530

    Initial Commit

---
+++ simple and Symmetric DDA/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  simple and Symmetric DDA
+//
+//  Created by B Raghu Ram Reddy  on 16/01/17.
+//  Copyright © 2017 B Raghu Ram Reddy . All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
